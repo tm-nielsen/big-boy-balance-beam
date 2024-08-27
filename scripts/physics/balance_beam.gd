9ba0eb96e854168cfbcdeeb0d6fa8ce3b114d73f
Twig Nielsen <littleb88888888@gmail.com>
2024-08-26 20:36:25 -0600

    Improved beam drawing - added stroke offset to top points of balance beam shape - adjusted z indexes of visual elements to draw beam between character bases and path sets - adjusted player size and physics parameters

--- scripts/physics/balance_beam.gd
+++ scripts/physics/balance_beam.gd
@@ -39,12 +39,14 @@ func _draw():
   var right_end = Vector2.RIGHT * width / 2
   var left_end = Vector2.LEFT * width / 2
   var bottom = Vector2.DOWN * height
+  var stroke_offset = Vector2.DOWN * stroke_width / 2
 
-  shape_points.push_back(right_end)
+  shape_points.push_back(right_end + stroke_offset)
   shape_points.push_back(right_end + bottom / 2)
   shape_points.push_back(bottom)
   shape_points.push_back(left_end + bottom / 2)
-  shape_points.push_back(left_end)
+  shape_points.push_back(left_end + stroke_offset)
+
   ShapeDrawer.draw_shape(self, shape_points, fill_colour, stroke_width, stroke_colour)
 
 func _physics_process(delta: float):
