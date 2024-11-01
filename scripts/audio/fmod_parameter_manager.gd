c94e603a2b60457245c6cd57b4c657800f551e34
Twig Nielsen <littleb88888888@gmail.com>
2024-10-31 19:18:30 -0600

    Refactored music to use event instance

--- scripts/audio/fmod_parameter_manager.gd
+++ scripts/audio/fmod_parameter_manager.gd
@@ -2,6 +2,27 @@ class_name FmodParameterManager
 extends Node
 
 @export var print_logs: bool = false
+@export var event_name: String
+
+var event_instance: FmodEvent
+
+
+func _ready():
+  if event_name:
+    event_instance = FmodServer.create_event_instance('event:/%s' % event_name)
+    event_instance.start()
+
+
+func set_parameter(paramater_name: String, label: String):
+  event_instance.set_parameter_by_name_with_label(paramater_name, label, false)
+  if print_logs:
+    print('setting fmod event paramater "%s" with label "%s"' % [paramater_name, label])
+
+func set_parameter_by_value(parameter_name: String, value: float):
+  event_instance.set_parameter_by_name(parameter_name, value)
+  if print_logs:
+    print('setting fmod event parameter "%s" with value "%s"' % [parameter_name, value])
+
 
 func set_global_parameter(parameter_name: String, label: String):
   FmodServer.set_global_parameter_by_name_with_label(parameter_name, label)
--- scripts/audio/music_manager.gd
+++ scripts/audio/music_manager.gd
@@ -7,7 +7,7 @@ var player_scores: Array[int] = [0, 0]
 
 
 func _ready():
-	FmodServer.create_event_instance("event:/StageMusic").start()
+	super()
 	GameObserver.game_state_changed.connect(_on_game_state_changed)
 	GameObserver.player_scored.connect(_on_player_scored)
 	GameObserver.round_won.connect(_on_round_won)
@@ -16,26 +16,26 @@ func _ready():
 func _on_game_state_changed(new_state: GameState):
 	match new_state:
 		GameState.CHARACTER_SELECTION:
-			set_global_parameter('GameState', 'CharacterSelect')
-			set_global_parameter_by_value('RoundWinner', 0)
-			set_global_parameter_by_value('PointWinner', 0)
-			set_global_parameter_by_value('Player1Score', 0)
-			set_global_parameter_by_value('Player2Score', 0)
-			set_global_parameter('RoundWon', 'No')
-			set_global_parameter('PointWon', 'No')
+			set_parameter('GameState', 'CharacterSelect')
+			set_parameter_by_value('RoundWinner', 0)
+			set_parameter_by_value('PointWinner', 0)
+			set_parameter_by_value('Player1Score', 0)
+			set_parameter_by_value('Player2Score', 0)
+			set_parameter('RoundWon', 'No')
+			set_parameter('PointWon', 'No')
 		GameState.GAMEPLAY:
-			set_global_parameter('GameState', 'Gameplay')
+			set_parameter('GameState', 'Gameplay')
 		GameState.RESETTING:
-			set_global_parameter('GameState', 'Resetting')
-			set_global_parameter('PointWon', 'No')
+			set_parameter('GameState', 'Resetting')
+			set_parameter('PointWon', 'No')
 
 func _on_player_scored(player_index: int):
 	player_scores[player_index] += 1
-	set_global_parameter_by_value('Player%dScore' % (player_index + 1), player_scores[player_index])
-	set_global_parameter_by_value('PointWinner', player_index + 1)
-	set_global_parameter('PointWon', 'Yes')
+	set_parameter_by_value('Player%dScore' % (player_index + 1), player_scores[player_index])
+	set_parameter_by_value('PointWinner', player_index + 1)
+	set_parameter('PointWon', 'Yes')
 
 func _on_round_won(winner_index: int):
-	set_global_parameter_by_value('RoundWinner', winner_index + 1)
-	set_global_parameter('RoundWon', 'Yes')
+	set_parameter_by_value('RoundWinner', winner_index + 1)
+	set_parameter('RoundWon', 'Yes')
 	player_scores = [0, 0]
